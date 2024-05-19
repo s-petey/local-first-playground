a5ef11851eb87bb9133581b38cec458647161507
Sam <samllama55@gmail.com>
2024-05-19 11:00:27 +0200

    ADD: Svelte eslint config. ADD: Triplit app example starter.

---
+++ packages/eslint-config/svelte.js
@@ -0,0 +1,47 @@
+const { resolve } = require('node:path');
+
+const project = resolve(process.cwd(), 'tsconfig.json');
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'plugin:svelte/recommended',
+    'eslint-config-turbo',
+  ],
+  env: {
+    node: true,
+    browser: true,
+  },
+  plugins: ['only-warn'],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    '.*.js',
+    'svelte.config.js',
+    'node_modules/',
+  ],
+
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project,
+    extraFileExtensions: ['.svelte'], // This is a required setting in `@typescript-eslint/parser` v4.24.0.
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      // Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+    },
+  ],
+};
